e266a5ade3d18daa156f8e85832eb192e8ee03d5
Michaelson, Eitan <eitan.michaelson@intel.com>
2025-06-05 21:04:29 +0300

    Updates

--- src/auto_forge/resources/shared/bootstrap.sh
+++ src/auto_forge/resources/shared/bootstrap.sh
@@ -134,7 +134,7 @@ main() {
 	# Install AutoForge using pip
 	clear
 	echo -ne '\e[?25l' # Hide cursor
-	printf "\n\nPlease wait while AutoForge is being downloaded and installed...\r"
+	printf "\n\nPlease wait while 🛠️ AutoForge is being downloaded and installed...\r"
 	install_autoforge || return 1
 
 	# Construct AutoForge command
