e4fa3cda37c08143b2ee718d963a9110d8a2aeea
Michaelson, Eitan <eitan.michaelson@intel.com>
2025-06-11 02:00:55 +0300

    Updates

--- src/auto_forge/builders/cmake_builder.py
+++ src/auto_forge/builders/cmake_builder.py
@@ -13,6 +13,7 @@ Classes:
 
 import logging
 import os
+from enum import Enum, auto
 from pathlib import Path
 from typing import Any
 from typing import Optional
@@ -27,6 +28,15 @@ from auto_forge import (BuilderRunnerInterface, BuilderToolChain, BuildProfileTy
 AUTO_FORGE_MODULE_NAME = "cmake"
 AUTO_FORGE_MODULE_DESCRIPTION = "CMake builder"
 AUTO_FORGE_MODULE_VERSION = "1.0"
+
+
+class _CMakeBuildStep(Enum):
+    PRE_CONFIGURE = auto()
+    CONFIGURE = auto()  # cmake -S . -B build/ ...
+    PRE_BUILD = auto()
+    BUILD = auto()  # ninja (or cmake --build)
+    POST_BUILD = auto()
+    DONE_BUILD = auto()
 
 
 # noinspection DuplicatedCode
@@ -48,6 +58,7 @@ class CMakeBuilder(BuilderRunnerInterface):
         self._environment: Optional[CoreEnvironment] = None
         self._prompt: Optional[CorePrompt] = None
         self._toolchain: Optional[BuilderToolChain] = None
+        self._state: _CMakeBuildStep = _CMakeBuildStep.PRE_CONFIGURE
 
         super().__init__(build_system=AUTO_FORGE_MODULE_NAME)
 
@@ -88,6 +99,7 @@ class CMakeBuilder(BuilderRunnerInterface):
             It encapsulates a critical, tightly-coupled sequence of logic that benefits from being kept together
             for clarity, atomicity, and maintainability. Refactoring would obscure the execution flow.
         """
+
         config = build_profile.config_data
         self._environment = CoreEnvironment.get_instance()
 
@@ -101,6 +113,9 @@ class CMakeBuilder(BuilderRunnerInterface):
         for field in mandatory_required_fields:
             if field not in config:
                 raise ValueError(f"missing mandatory field in configuration: '{field}'")
+
+        # Update step and optionally handle extra argumnets based on the current state
+        self._set_state(build_state=_CMakeBuildStep.PRE_CONFIGURE, extra_args=build_profile.extra_args, config=config)
 
         # Get optional 'execute_from' property and validate it
         execute_from = config.get("execute_from", None)
@@ -146,11 +161,6 @@ class CMakeBuilder(BuilderRunnerInterface):
         compiler_options = config.get("compiler_options")
         artifacts: Optional[list[str]] = config.get("artifacts", None)
 
-        # Process optional out of build hierarchy argumnets
-        return_code = self._process_extra_args(extra_args=build_profile.extra_args, config=config)
-        if return_code != 0:
-            return return_code
-
         # Merge cmake specific options from the tool chain with the build configuration options into  single list
         if cmake_options and compiler_options:
             merged_options = cmake_options + compiler_options
@@ -163,7 +173,12 @@ class CMakeBuilder(BuilderRunnerInterface):
         # Execute CMake, note that pending on the compilation options this could a single run or the first run
         # out of 2 when building with Ninja.
         try:
-            self.print_message(message=f"Executing build in '{execute_from}'")
+
+            # Update step and optionally handle extra argumnets based on the current state
+            self._set_state(build_state=_CMakeBuildStep.PRE_CONFIGURE, extra_args=build_profile.extra_args,
+                            config=config)
+
+            self.print_message(message=f"Configuring in '{execute_from}'")
             results = self._environment.execute_shell_command(command_and_args=command_line,
                                                               echo_type=TerminalEchoType.LINE,
                                                               cwd=str(execute_from),
@@ -172,12 +187,19 @@ class CMakeBuilder(BuilderRunnerInterface):
         except Exception as execution_error:
             raise RuntimeError(f"build process failed to start: {execution_error}") from execution_error
 
-        # Validate CMaKE results
+        # Validate CMake results
         self.print_build_results(results=results, raise_exception=True)
+
+        # Update step and optionally handle extra argumnets based on the current state
+        self._set_state(build_state=_CMakeBuildStep.PRE_BUILD, extra_args=build_profile.extra_args, config=config)
 
         # Check if the previous step was configuration and if so verify that we have Ninja
         if is_config_step and ninja_build_command is not None:
             try:
+
+                # Update step and optionally handle extra argumnets based on the current state
+                self._set_state(build_state=_CMakeBuildStep.BUILD, extra_args=build_profile.extra_args, config=config)
+
                 ninja_command_line = f"{ninja_build_command} -C {str(build_path)}"
                 results = self._environment.execute_shell_command(command_and_args=ninja_command_line,
                                                                   echo_type=TerminalEchoType.LINE,
@@ -188,6 +210,9 @@ class CMakeBuilder(BuilderRunnerInterface):
 
             # Validate CMaKE results
             self.print_build_results(results=results, raise_exception=True)
+
+            # Update step and optionally handle extra argumnets based on the current state
+            self._set_state(build_state=_CMakeBuildStep.POST_BUILD, extra_args=build_profile.extra_args, config=config)
 
         # Process post build steps if specified
         steps_data: Optional[dict[str, str]] = config.get("post_build_steps", {})
@@ -211,17 +236,25 @@ class CMakeBuilder(BuilderRunnerInterface):
             raise ValueError("missing expected build artifacts:" + "\n".join(missing_artifacts))
 
         self.print_message(message=f"Building of '{build_target_string}' was successful", log_level=logging.INFO)
+
+        # Update step and optionally handle extra argumnets based on the current state
+        self._set_state(build_state=_CMakeBuildStep.DONE_BUILD, extra_args=build_profile.extra_args, config=config)
+
         return results.return_code
 
-    def _process_extra_args(self, extra_args: Optional[list[str]] = None,
-                            config: Optional[dict[str, Any]] = None) -> int:
+    def _set_state(self, build_state: _CMakeBuildStep,
+                   extra_args: Optional[list[str]] = None,
+                   config: Optional[dict[str, Any]] = None) -> int:
         """
 
         """
+        self._state = build_state
+
         for arg in extra_args:
-            if arg in ("--clean", "--clean_exit"):
+            if arg == "--clean":
                 clean_command: Optional[str] = config.get("clean", None)
-                if isinstance(clean_command, str):
+                if isinstance(clean_command, str) and build_state == _CMakeBuildStep.PRE_BUILD:
+                    extra_args.remove("--clean")
                     return self._execute_single_step(command=clean_command, name=arg)
 
         return 0
